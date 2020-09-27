3508711dcf3228dbbd8a8f046302745a9b0b6445
Kartik Pandya <kpandya@manh.com>
2020-09-26 21:32:36 -0400

    kpandya initial commit

---
+++ src/main/java/app/kartik/sunderban/SunderbanApplication.java
@@ -0,0 +1,13 @@
+package app.kartik.sunderban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunderbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunderbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/kartik/sunderban/SunderbanApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kartik.sunderban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunderbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
